3a33c33a2409c42e3963d1946e10e534c91a35f6
Jacobo Zonana <jacobozonana@gmail.com>
2020-08-08 22:40:52 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var a = prompt("Eres Hombre o Mujer?")
+var b = parseInt(prompt("Cuantos años tienes?"))
+var c = 13
+
+if (b < c || a != "hombre"){alert("Mas suerte para la proxima!")}
+
+else{alert("Bienvenido!!")}
